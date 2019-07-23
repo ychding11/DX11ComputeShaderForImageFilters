3647d2b412baada4c349c829272b29c3757426f2
Yaochuang <ych_ding@163.com>
2019-07-23 23:20:10 +0800

    temp

--- framework/GHIResources.h
+++ framework/GHIResources.h
@@ -17,31 +17,56 @@ namespace SimpleFramework
 
 	};
 
-	enum EUAVDemension
+	enum EViewDemension
 	{
-        UAVDimension_TEXTURE2D,
+        EViewDimension_TEXTURE2D,
 
 	};
+
+    enum EResourceUsage
+    {
+        Default	= 0,
+        Immutable = 1,
+        Dynamic	= 2,
+        Staging	= 3
+    };
+
+    class TextureDesc2D
+    {
+	public:
+		uint32_t Width = 0;
+		uint32_t Height = 0;
+		uint32_t MipLevels = 1;
+		uint32_t ArraySize = 1;
+		EPixelFormat Format = PixelFormat_R8G8B8A8_UNORM;
+		uint32_t SampleCountPixel = 1;
+		uint32_t ImageQualityLevel = 0;
+		EResourceUsage Usage = EResourceUsage::Default;
+		uint32_t BindFlags = 0;
+		uint32_t CPUAccessFlags = 0;
+		uint32_t MiscFlags = 0;
+    };
 
     struct GHIUAVParam
     {
         EPixelFormat Format;
-        EUAVDemension ViewDimension;
+        EViewDemension ViewDimension;
 		uint32_t MostDetailedMip;
 		uint32_t MipLevels;
     };
-    struct GHISRVParam
+    class GHISRVParam
     {
-        EPixelFormat Format;
-        EUAVDemension ViewDimension;
-		uint32_t MostDetailedMip;
-		uint32_t MipLevels;
+	public:
+        EPixelFormat Format = PixelFormat_R8G8B8A8_UNORM;
+        EViewDemension ViewDimension = EViewDimension_TEXTURE2D;
+		uint32_t MostDetailedMip = 0;
+		uint32_t MipLevels = 1;
 
     };
     struct GHIRTVParam
     {
         EPixelFormat Format;
-        EUAVDemension ViewDimension;
+        EViewDemension ViewDimension;
 		uint32_t MostDetailedMip;
 		uint32_t MipLevels;
     };
