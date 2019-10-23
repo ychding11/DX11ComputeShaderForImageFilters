a1e79e86334a908cc5e89b00027cdcced0e1b458
Yaochuang <yaochuang.ding@autodesk.com>
2019-10-23 10:55:58 +0800

    There is a bug in checker board.

--- data/sphere.hlsl
+++ data/sphere.hlsl
@@ -83,7 +83,7 @@ float sphSoftShadow( in float3 ro, in float3 rd, in float4 sph, in float k )
     float b = dot( oc, rd );
     float c = dot( oc, oc ) - sph.w*sph.w;
     float h = b*b - c;
-    float esp = 1e-4;
+    float esp = 1e-5;
 #if 1
     // physically plausible shadow
     float d = sqrt( max(0.0,sph.w*sph.w-h)) - sph.w;
@@ -119,8 +119,6 @@ float iPlane( in float3 ro, in float3 rd )
     return (-1.0 - ro.y)/rd.y;
 }
 
-
-
 void animateCamera( out float3 origin, out float3 target )
 {
 	float an = 0.1*sin(0.1*cTime);
@@ -150,36 +148,119 @@ void calcRayForPixel( in float3 samplePoint, out float3 resRo, out float3 resRd 
 }
 
 
-float2 texCoords( in float3 pos, int mid )
+//--------------------------------------------------------------------------------------
+// calculate the box filtered texture value by a tecture coordinate "p"
+//--------------------------------------------------------------------------------------
+float checkerboardBoxFiltered( in float2 p, in float2 ddx, in float2 ddy )
 {
-    vec2 matuv;
-    
-    if( mid==0 )
-    {
-        matuv = pos.xz;
-    }
-    else if( mid==1 )
-    {
-        float3 q = normalize( pos - sc0.xyz );
-        matuv = vec2( atan(q.x,q.z), acos(q.y ) )*sc0.w;
-    }
-    else if( mid==2 )
-    {
-        float3 q = normalize( pos - sc1.xyz );
-        matuv = vec2( atan(q.x,q.z), acos(q.y ) )*sc1.w;
-    }
-    else if( mid==3 )
-    {
-        float3 q = normalize( pos - sc2.xyz );
-        matuv = vec2( atan(q.x,q.z), acos(q.y ) )*sc2.w;
-    }
-    else if( mid==4 )
-    {
-        float3 q = normalize( pos - sc3.xyz );
-        matuv = vec2( atan(q.x,q.z), acos(q.y ) )*sc3.w;
-    }
+    // filter kernel
+    float2 w = max(abs(ddx), abs(ddy)) + 0.01;  
+	
+    // analytical integral (box filter)
+    float2 i = 2.0*(abs(frac((p-0.5*w)/2.0)-0.5)-abs(frac((p+0.5*w)/2.0)-0.5))/w;
+	
+    // xor pattern
+    return 0.5 - 0.5*i.x*i.y;                  
+}
 
-	return 12.0*matuv;
+//--------------------------------------------------------------------------------------
+// calculate the raw texture value by a tecture coordinate "uv"
+//--------------------------------------------------------------------------------------
+float checkerboard( in float2 uv )
+{
+    float2 q = floor(uv);
+    return fmod( q.x+q.y, 2.0 );    // xor pattern
+}
+
+float3 animateDirectLight()
+{
+	float pi = 3.141592;
+    float delta = pi / 99999;
+	float theta = 1.75*pi;
+	if (cAnimateLight == 1)
+	{
+		int i = int(cTime) % 99998;
+		theta = pi + delta * i;
+	}
+	float3 lig = normalize( float3(cos(theta),sin(theta),0));
+	return lig;
+}
+
+float4 animateSphere()
+{
+	float4 sph = float4(float3(0,0.0,1.0), 1.0 );
+	if (cAnimateSphere == 1)
+		sph = float4(cos(cTime)*float3(0,0.0,1.0), 1.0 );
+    sph.x = 1.0;   
+	return sph;
+}
+
+static float4 mysphere;
+//--------------------------------------------------------------------------------------
+// hit test: ray(ro, rd) with primitives in scene
+//         return (distance, objectID)
+//--------------------------------------------------------------------------------------
+float2  worldIntersect( in float3 ro, in float3 rd, in float maxlen )
+{
+    float tmin = 1e10;
+    float2 ret = float2(tmin, -1.0);
+    
+    float t1 = iPlane( ro, rd );
+    if( t1>0.0 )
+    {
+        tmin = t1;
+		ret.y = 0;
+    }
+#if 1
+    float t2 = sphIntersect( ro, rd, mysphere );
+    if( t2>0.0 && t2<tmin )
+    {
+        tmin = t2;
+		ret.y = 1;
+	}
+#endif 
+
+    ret.x = tmin;
+	if (tmin > maxlen) ret.y = -1.0;
+	return ret;
+}
+
+//--------------------------------------------------------------------------------------
+// calculate the texture coordinate at a specified "hit-point" on sphere surface
+// n is the unit normal on hit point
+//--------------------------------------------------------------------------------------
+float2 calcSphereTexCoord( in float3 n)
+{
+    float2 uv = float2( atan2(n.x, n.z), acos(n.y ));
+	return 12.0*uv;
+}
+
+
+float3 Shading( in float3 hitpoint, in float3 lightdir, in float objectID )
+{
+	float3 col = cLightColor; //light intensity.
+	float3 hitnormal = float3(0,0,0);
+	float  occ = 1.0; // 1.0 means no occlude
+	
+	if (objectID == 0)
+	{	
+		hitnormal = float3(0,1,0);
+		occ = 1.0-sphOcclusion( hitpoint, hitnormal, mysphere );
+	}
+	else if (objectID == 1)
+    {
+		hitnormal = sphNormal( hitpoint, mysphere );
+		occ = 0.5 + 0.5*hitnormal.y;
+		float2 uv = calcSphereTexCoord(hitnormal);
+	    float c = checkerboard( uv );
+	    col *= c;
+	}
+	
+	
+    col *= clamp( dot(hitnormal,-lightdir), 0.0, 1.0 );
+    col *= sphSoftShadow( hitpoint, lightdir, mysphere, 2.0 );
+    col += 0.05*occ; // AO
+	return col;
 }
 
 //--------------------------------------------------------------------------------------
@@ -192,63 +273,20 @@ float4 PSSphere( PS_INPUT input) : SV_Target
     float2 p = (2.0*pixelCoord-resolution.xy) / resolution.y;
     
 	// "camera" setting
-	float3 ro = float3(0.0, 0.0, 6.0 );
+	float3 ro = float3(0.0, 0.0, 6.0 )
 	float3 rd = normalize( float3(p,-2.0) );
 	
-    // sphere animation
-	float4 sph = float4(float3(0,0.0,1.0), 1.0 );
-	if (cAnimateSphere == 1)
-		sph = float4(cos(cTime)*float3(0,0.0,1.0), 1.0 );
-    sph.x = 1.0;   
+	mysphere = animateSphere();
 	
-	// directional light setting
-	float pi = 3.141592;
-    float delta = pi / 99999;
-	float theta = 1.75*pi;
-	if (cAnimateLight == 1)
-	{
-		int i = int(cTime) % 99998;
-		theta = pi + delta * i;
-	}
-	float3 lig = normalize( float3(cos(theta),sin(theta),0));
-	
-	// render
     float3 col = float3(0,0,0);
 
-    float tmin = 1e10;
-    float3 nor = float3(0,1,0);
-    float  occ = 1.0; // 1.0 means no occlude
-    
-    float t1 = iPlane( ro, rd );
-    if( t1>0.0 )
-    {
-        tmin = t1;
-        float3 pos = ro + t1*rd;
-        float3 nor = float3(0,1,0);
-        occ = 1.0-sphOcclusion( pos, nor, sph );
-    }
-#if 1
-    float t2 = sphIntersect( ro, rd, sph );
-    if( t2>0.0 && t2<tmin )
-    {
-        tmin = t2;
-        float3 pos = ro + t2*rd;
-        nor = sphNormal( pos, sph );
-        occ = 0.5 + 0.5*nor.y;
+    float2 ret = worldIntersect( ro, rd, 1e4 );
+	if (ret.y >= 0)
+	{
+	  col = Shading( ro + ret.x*rd, animateDirectLight(), ret.y );
+	  col *= exp( -0.05*ret.x );
 	}
-#endif 
-
-    if( tmin<1e4 ) // Not infinity
-    {
-        float3 pos = ro + tmin*rd;
-        
-		col = cLightColor; //float3(1.0,1.0,1.0); //light intensity.
-        col *= clamp( dot(nor,-lig), 0.0, 1.0 );
-        col *= sphSoftShadow( pos, lig, sph, 2.0 );
-        col += 0.05*occ; // AO
-	    col *= exp( -0.05*tmin );
-    }
-
+	
     col = sqrt(col);
     return float4( col, 1.0 );
 }
